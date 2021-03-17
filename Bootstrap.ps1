b8bd46f0af899590ddcfce816289f6b723b875c3
Daniel Falkner <dafalkne@microsoft.com>
2021-03-17 08:16:16 +0100

    Force TLS12

--- Bootstrap.ps1
+++ Bootstrap.ps1
@@ -1,3 +1,6 @@
+#Force TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+
 #Install Chocolatey
 Set-ExecutionPolicy Bypass -Scope Process -Force
 Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
